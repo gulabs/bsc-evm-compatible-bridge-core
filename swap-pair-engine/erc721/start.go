260b36350571e948f0dd8452b1c406b90bb82da6
synycboom <synycboom@gmail.com>
2021-11-04 23:54:01 +0700

    fix: create track retry is not increased

---
+++ swap-pair-engine/erc721/start.go
@@ -0,0 +1,40 @@
+package engine
+
+import (
+	"reflect"
+	"runtime"
+	"time"
+
+	"github.com/synycboom/bsc-evm-compatible-bridge-core/util"
+)
+
+const (
+	watchEventDelay = time.Duration(5) * time.Second
+)
+
+func (e *Engine) Start() {
+	// SwapPair
+	go e.run(e.manageOngoingRegistration, watchRegisterEventDelay)
+	go e.run(e.manageConfirmedRegitration, watchRegisterEventDelay)
+	go e.run(e.manageTxCreatedRegistration, watchRegisterEventDelay)
+	go e.run(e.manageTxSentRegistration, watchRegisterEventDelay)
+}
+
+func (e *Engine) run(fn func(), delay time.Duration) {
+	fnName := runtime.FuncForPC(reflect.ValueOf(fn).Pointer()).Name()
+	if delay.Seconds() == 0 {
+		delay = watchEventDelay
+	}
+
+	for {
+		time.Sleep(watchEventDelay)
+
+		if e.deps.Recorder[e.chainID()].LatestBlockCached() == nil {
+			util.Logger.Infof("[Engine.run][%s]: no latest block cache found for chain id %s", fnName, e.chainID())
+
+			continue
+		}
+
+		fn()
+	}
+}
